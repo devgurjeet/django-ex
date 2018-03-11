58a0e7862d800ed620026672bf890670cf1cda5d
Gurjeet Singh <developer.gurjeet@gmail.com>
2018-03-11 15:44:35 +0530

    changes to demos

--- demo/urls.py
+++ demo/urls.py
@@ -1,7 +1,12 @@
 from django.conf.urls import include, url
-from demo.views import index
+from demo.views import index,testing, addDemo, getDemo, deleteDemos
 
 urlpatterns = [
     url(r'^$', index, name='index'),
+    url(r'^testing/$', testing, name='testing'),
+    url(r'^add/$', addDemo, name='add-demo'),
+    url(r'^get/$', getDemo, name='get-demo'),
+    url(r'^delete/$', deleteDemos, name='delete-demo'),
+
     # url(r'^blog/', include('blog.urls')),
  ]
--- demo/views.py
+++ demo/views.py
@@ -1,8 +1,34 @@
+import random
+
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
+from django.core import serializers
 
+
+from demo.models import Demo
 def index(request):
     data = {'message': 'Hello World'}
     # return HttpResponse(data)
     return JsonResponse(data)
 
+def testing(request):
+    data = {"message":"This is a test Route."}
+    return JsonResponse(data)
+
+def addDemo(request):
+    name = "Demo: "+str(random.randint(1,101))
+    d = Demo(name=name, description="This is a testing "+name)
+    d.save()
+    response = {"message": "Demo create Successfully"}
+    return JsonResponse(response)
+
+def getDemo(request):
+    demos = Demo.objects.all()
+    return render(request, 'demo/index.html', {
+        "demos": demos
+    })
+
+def deleteDemos(request):
+    demos = Demo.objects.all().delete()
+    response = {"message": "Demo deleted Successfully"}
+    return JsonResponse(response)
